f3ec19cf6151c7791843d18c94f31d123138886e
acher0nparth <thackerb6@gmail.com>
2020-03-10 18:45:27 -0400

    added a comment

--- Lab6.java
+++ Lab6.java
@@ -1,5 +1,6 @@
 public class Lab6 {
     public static void main(String[] args){
         System.out.println("git is very annoying to use properly!");
+        //adding comment for step 6 of the lab
     }
 }
